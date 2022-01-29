bc0943a666f67e18c9dba90f596570c0b02f03b2
Lkhooton <lkhooton@gmail.com>
2022-01-29 10:06:27 -0500

    halfway

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ const game = {
   prevGuesses: [],
   getGuess: function(){
     while (this.playerChoice !== this.secretNum){
-      let playerChoice = prompt (`Enter a guess between ${smallestNum} and ${biggestNum}.`)
+      let playerChoice = prompt (`Enter a guess between ${smallestNum} and ${biggestNum}.`);
+      let wholeNumber = parseInt(this.playerChoice);
+      if(this.wholeNumber > this.biggestNum){
+        let wholeNumber = 100;
+      }
+      if (this.wholeNumber < this.smallestNum){
+        let wholeNumber = 1;
+      }
+      if (typeof this.wholeNumber !== 'number'){
+        let wholeNumber = Math.floor(Math.random() * 100)
+      }
     }
   }
 }
