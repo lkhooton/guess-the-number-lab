aad1a193f0f5a0c45cbb7ee4762e385391a80e97
Lkhooton <lkhooton@gmail.com>
2022-01-30 09:42:48 -0500

    Step 5 add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
+  playerChoice: null,
   secretNum: null,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
@@ -15,7 +16,7 @@ const game = {
   prevGuesses: [],
   getGuess: function(){
     while (this.playerChoice !== this.secretNum){
-      let playerChoice = prompt (`Enter a guess between ${smallestNum} and ${biggestNum}.`);
+      let playerChoice = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`);
       let wholeNumber = parseInt(this.playerChoice);
       if(this.wholeNumber > this.biggestNum){
         let wholeNumber = 100;
@@ -26,7 +27,21 @@ const game = {
       if (typeof this.wholeNumber !== 'number'){
         let wholeNumber = Math.floor(Math.random() * 100)
       }
+      render = function(){
+        if (this.wholenNumber === secretNum){
+          alert (`Congrats! you guessed the number in ${game.prevGuesses.length} guesses!!
+            Previous guesses: ${prevGuesses}`);
+        }else if (this.wholeNumber > this.secretNum){
+          alert (`Your guess it too high!
+          Previous guesses: ${this.prevGuesses}`);
+          game.guess();
+        }else if (this.wholeNumber < this.secretNum){
+          alert(`Your guess is too low!!
+          Previous guesses: ${this.prevGuesses}`);
+          game.guess();
+        }
+      }
     }
   }
 }
-console.log(game)
+game.play()
