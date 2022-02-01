93191801bb7b7709e2abf0cbfa155b21752f31b0
Lkhooton <lkhooton@gmail.com>
2022-02-01 17:11:31 -0500

    almost done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 4,
   smallestNum: 1,
   playerChoice: null,
   secretNum: null,
@@ -8,49 +8,31 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
       game.getGuess();
-      this.prevGuesses.push(this.wholeNumber);
-      if (typeof(this.wholeNumber) === 'number' && this.wholeNumber !== this.secretNum){
-        game.render();
-      }
-  },
-  prevGuesses: [],
-  getGuess : function(){
-    while (this.playerChoice !== this.secretNum){
-      let playerChoice = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`);
-      let wholeNumber = parseInt(this.playerChoice);
-      if(this.wholeNumber > this.biggestNum){
-        let wholeNumber = 100;
-      }
-      if (this.wholeNumber < this.smallestNum){
-        let wholeNumber = 1;
-      }
-      if (typeof this.wholeNumber !== 'number'){
-        let wholeNumber = Math.floor(Math.random() * 100)
-      }
-      if (this.wholeNumber === this.secretNum) break;//try to get while loop to stop
-    } 
-  },//confused on while loops. Not sure how to end it. 
-
-
-
-//Trouble connecting everything together
+        game.render(); 
+      },
+      prevGuesses: [],
+      getGuess : function(){
+        while (this.playerChoice !== this.secretNum){
+          this.playerChoice = parseInt(prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`));
+          this.prevGuesses.push(this.playerChoice);
+        } 
+    },
+  
   render: function(){
-       if (this.wholeNumber === secretNum){
-          prompt (`Congrats! you guessed the number in ${game.prevGuesses.length} guesses!!
-            Previous guesses: ${prevGuesses}`);
-        }if (this.wholeNumber > this.secretNum){
+       if (this.playerChoice === this.secretNum){
+          prompt (`Congrats! you guessed the number in ${this.prevGuesses.length} guesses!!
+            Previous guesses: ${this.prevGuesses}`);
+        } if (this.playerChoice > this.secretNum){
           prompt (`Your guess it too high!
           Previous guesses: ${this.prevGuesses}`);
           game.guess();
-        }if (this.wholeNumber < this.secretNum){
+        } if (this.playerChoice < this.secretNum){
           prompt (`Your guess is too low!!
           Previous guesses: ${this.prevGuesses}`);
           game.guess();
-          
-        
         }
-}
-}
+      }
+} 
 game.play()
 
 
